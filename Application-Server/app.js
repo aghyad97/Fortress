cebb04ec1272a48d14d9ba745c92544704619b38
aghyad97 <aaa1997aaa@gmail.com>
2020-10-16 15:08:06 +0400

    add bootstrap and layout

--- Application-Server/app.js
+++ Application-Server/app.js
@@ -35,8 +35,7 @@ if(process.env.NODE_ENV !== "test") {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-
+app.use('/static', express.static(path.join(__dirname, "public")));
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
