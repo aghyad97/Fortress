d31d04af4f2b0113e2c3ad2cdb393ac7864da302
aghyad97 <aaa1997aaa@gmail.com>
2020-12-08 07:32:33 +0400

    change layout of the dashboard

--- Application-Server/public/js/dashboard.js
+++ Application-Server/public/js/dashboard.js
@@ -1,5 +1,42 @@
-$(function () {
+var wsbroker = "broker.hivemq.com"; //mqtt websocket enabled broker
+var wsport = 443 // port for above
 
+// create client using the Paho library
+var client = new Paho.MQTT.Client(wsbroker, wsport, "clientId");
+var options = {
+  timeout: 3,
+  onSuccess: function () {
+    console.log("mqtt connected");
+    client.subscribe("project/images", {
+      qos: 1
+    });
+  },
+  onFailure: function (message) {
+    console.log("Connection failed: " + message.errorMessage);
+  }
+};
+
+$(function () {
+  client.connect(options);
+
+  client.onMessageArrived = function (message) {
+    console.log(message);
+    var json = JSON.parse(message.payloadString);
+    console.log(json);
+    alert(json);
+    $.ajax({
+      type: 'GET',
+      url: '/api/getimages?isPredict=true',
+      success: function (data) {
+        new Splide( '.splide1' ).mount();
+        console.log();
+        let json = JSON.parse(data['data']);
+        for ( var i = 0, len = json.length; i < len; i++ ) {
+          $('.splide__list1').append('<li class="splide__slide1"><div class="splide__slide__container"><img src="data:image/png;base64, ' + json[i]['image'] + '"/></div></li>')
+        }
+      },
+    });
+  };
   // toggle system
   $("#system-toggle").click(function () {
     $.ajax({
@@ -17,17 +54,6 @@ $(function () {
     });
   });
 
-  // sensor data
-  window.setInterval(function () {
-
-  }, 5000);
-
-  // notifications
-  window.setInterval(function () {
-
-  }, 1000);
-
-
   // pictures of the camera
   $("#getImages").click(function () {
     console.log($('#picturesTime').val());
@@ -36,9 +62,8 @@ $(function () {
         type: 'GET',
         url: '/api/getimages?limit=' +  parseInt($('#picturesTime').val()),
         success: function (data) {
-          console.log();
-          let json = JSON.parse(data['data']);
           new Splide( '.splide' ).mount();
+          let json = JSON.parse(data['data']);
           for ( var i = 0, len = json.length; i < len; i++ ) {
             $('.splide__list').append('<li class="splide__slide"><div class="splide__slide__container"><img src="data:image/png;base64, ' + json[i]['image'] + '"/></div></li>')
           }
