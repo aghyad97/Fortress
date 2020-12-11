52da3555e4f01f1d53efd427d87ac32175175155
aghyad97 <aaa1997aaa@gmail.com>
2020-12-11 17:05:19 +0400

    added chart for sensors data with different inputs

--- Application-Server/public/js/dashboard.js
+++ Application-Server/public/js/dashboard.js
@@ -1,6 +1,7 @@
 var wsbroker = "broker.hivemq.com"; //mqtt websocket enabled broker
 var wsport = 443 // port for above
-
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
 // create client using the Paho library
 var client = new Paho.MQTT.Client(wsbroker, wsport, "clientId");
 var options = {
@@ -16,6 +17,20 @@ var options = {
   }
 };
 
+function drawChart(data) {
+  var data = google.visualization.arrayToDataTable(data);
+
+  var options = {
+    title: 'Values are with respect to time',
+    curveType: 'function',
+    legend: { position: 'bottom' }
+  };
+
+  var chart = new google.visualization.LineChart(document.getElementById('sensorDataCanvas'));
+
+  chart.draw(data, options);
+}
+
 $(function () {
   client.connect(options);
   $.ajax({
@@ -24,13 +39,13 @@ $(function () {
     success: function (data) {
       console.log();
       let json = JSON.parse(data['data']);
-      for ( var i = 0, len = json.length; i < len; i++ ) {
+      for (var i = 0, len = json.length; i < len; i++) {
         $('.splide__list').append('<li class="splide__slide text-center"><div class="splide__slide__container"><img src="data:image/png;base64, ' + json[i]['image'] + '"/></div></li>')
       }
       splide.refresh();
     },
   });
-  
+
   client.onMessageArrived = function (message) {
     console.log(message);
     $('.notifcationsMessages').append('<p>Found person at ' + message + '</p>');
@@ -52,21 +67,21 @@ $(function () {
     });
   });
   // pictures of the camera
-  var splide = new Splide( '.splide' , {
-    type        : 'loop',
-    autoplay    : true,
+  var splide = new Splide('.splide', {
+    type: 'loop',
+    autoplay: true,
   }).mount();
   $("#getImages").click(function () {
     console.log($('#picturesTime').val());
     //$( ".splide__list" ).empty();
-    if(!isNaN(parseInt($('#picturesTime').val()))){       
-      $( ".splide__list" ).empty();
+    if (!isNaN(parseInt($('#picturesTime').val()))) {
+      $(".splide__list").empty();
       $.ajax({
         type: 'GET',
-        url: '/api/getimages?limit=' +  parseInt($('#picturesTime').val()),
+        url: '/api/getimages?limit=' + parseInt($('#picturesTime').val()),
         success: function (data) {
           let json = JSON.parse(data['data']);
-          for ( var i = 0, len = json.length; i < len; i++ ) {
+          for (var i = 0, len = json.length; i < len; i++) {
             $('.splide__list').append('<li class="splide__slide text-center"><div class="splide__slide__container"><img src="data:image/png;base64, ' + json[i]['image'] + '"/></div></li>')
           }
           splide.refresh();
@@ -80,14 +95,14 @@ $(function () {
 
   // predicted images
   window.setInterval(function () {
-    $( ".splide__list" ).empty();
+    $(".splide__list").empty();
     $.ajax({
       type: 'GET',
       url: '/api/getimages',
       success: function (data) {
         console.log();
         let json = JSON.parse(data['data']);
-        for ( var i = 0, len = json.length; i < len; i++ ) {
+        for (var i = 0, len = json.length; i < len; i++) {
           $('.splide__list').append('<li class="splide__slide text-center"><div class="splide__slide__container"><img src="data:image/png;base64, ' + json[i]['image'] + '"/></div></li>')
         }
         splide.refresh();
@@ -99,60 +114,46 @@ $(function () {
   $("#getSensorData").click(function () {
     let dataType = $('#sensorDataType[name="sensorDataType"]:checked').val();
     console.log(dataType);
-    if(!isNaN(parseInt($('#sensorData').val()))){       
+    if (!isNaN(parseInt($('#sensorData').val()))) {
       $.ajax({
         type: 'GET',
-        url: '/api/getSensorValue?limit=' +  parseInt($('#sensorData').val()),
-        success: function (data) {
-          console.log(data);
-          var ctx = document.getElementById('sensorDataCanvas').getContext('2d');
-          var myChart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [{
-                      x: new Date(),
-                      y: 1
-                    }, {
-                      t: new Date(),
-                      y: 10
-                    }],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }],
-                    xAxes: [{
-                      ticks: {
-                          beginAtZero: true
-                      }
-                  }]
-                }
+        url: '/api/getSensorValue?limit=' + parseInt($('#sensorData').val()),
+        success: function (values) {
+          let valuesJson = JSON.parse(values.data);
+          let array = new Array();
+          if(dataType == null || dataType === 'all'){
+            let a = ['time', 'x', 'y', 'z', 'proximity'];
+            array.push(a);
+            for (let index = 0; index < valuesJson.length; index++) {
+              const dataTypeX = valuesJson[index]['x'];
+              const dataTypeY = valuesJson[index]['y'];
+              const dataTypeZ = valuesJson[index]['z'];
+              const dataTypeProximity = valuesJson[index]['proximity'];
+              const dataTypeTime = valuesJson[index]['createdAt'];
+              // dict = {
+              //   x: Date.parse(dataTypeTime),
+              //   y: dataTypeValue
+              // }
+              a = [Date.parse(dataTypeTime), dataTypeX, dataTypeY, dataTypeZ, dataTypeProximity]
+              array.push(a);
             }
-        });
+          } else {
+            console.log('21212');
+            let a = ['time', dataType];
+            array.push(a);
+
+            for (let index = 0; index < valuesJson.length; index++) {
+              const dataTypeValue = valuesJson[index][dataType];
+              const dataTypeTime = valuesJson[index]['createdAt'];
+              // dict = {
+              //   x: Date.parse(dataTypeTime),
+              //   y: dataTypeValue
+              // }
+              a = [Date.parse(dataTypeTime), dataTypeValue]
+              array.push(a);
+            }
+          }
+          drawChart(array);
         },
       });
     } else {
@@ -161,3 +162,38 @@ $(function () {
   });
 
 });
+
+// var ctx = $('#sensorDataCanvas');
+//           new Chart(ctx, {
+//             type: 'line',
+//             data: [
+//               {
+//                 x: Date.now(),
+//                 y: 1
+//               },{
+//                 x: Date.now(),
+//                 y: 2
+//               },{
+//                 x: Date.now(),
+//                 y: 3
+//               }
+//             ],
+//             options: {
+//               title: {
+//                 display: true,
+//                 text: dataType + ' values along with time axis'
+//               },
+//               scales: {
+//                 yAxes: [{
+//                   ticks: {
+//                     beginAtZero: true,
+//                   }
+//                 }],
+//                 xAxes: [{
+//                   ticks: {
+//                     beginAtZero: true,
+//                   }
+//                 }]
+//               }
+//             }
+//           });
